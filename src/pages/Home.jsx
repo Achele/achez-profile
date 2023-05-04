39d9a818ca82237120c0740bd1bae8049af43136
Ojoachele Onuh <ejigboojoachele@gmail.com>
2023-05-04 01:47:38 +0100

    Update the structure of home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import About from "../components/About";
+import Footer from "../components/Footer";
+import Movies from "../components/Movies";
+import Work from "../components/Work";
+
+export default function Home() {
+  return (
+    <>
+      <>
+        <About />
+        <Movies />
+        <Work />
+      </>
+      <Footer />
+    </>
+  );
+}
