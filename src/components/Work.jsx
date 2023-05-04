a394ed249ea980b324a8ed917abdf609ef1ef5d9
Ojoachele Onuh <ejigboojoachele@gmail.com>
2023-05-04 01:47:14 +0100

    Add professional journey

---
+++ src/components/Work.jsx
@@ -0,0 +1,25 @@
+export default function Work() {
+  return (
+    <>
+      <h3 className="work-title">Professional History</h3>
+      <div className="work">
+        {/* <h3>Work</h3> */}
+        <div className="work1">
+          <div>
+            <h3>Ikon Facilities</h3>
+            <p>Communication specialist</p>
+          </div>
+          <p>2018 - present</p>
+        </div>
+
+        <div className="work1">
+          <div>
+            <h3>AltSchool Africa</h3>
+            <p>Frontend Developer</p>
+          </div>
+          <p>2022 - 2023</p>
+        </div>
+      </div>
+    </>
+  );
+}
